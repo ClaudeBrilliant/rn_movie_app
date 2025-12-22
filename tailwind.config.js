589c2345a1b3889320b098229aa93f1db371d341
Claude Brilliant <claudennyongesa7@gmail.com>
2025-12-22 11:56:10 -0800

    recent configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App/**/*.{js,jsx,ts,tsx}"],
+  content: ["./App/**/*.{js,jsx,ts,tsx}","./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
-        primary: "#1E40AF",
+        primary: "#34323d",
         secondary: "#151312",
         light:{
           100: "#D6C6FF",
