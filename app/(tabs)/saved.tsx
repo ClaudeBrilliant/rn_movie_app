e15e2cf019106fab04850eeec2a6736ced074e08
Claude Brilliant <claudennyongesa7@gmail.com>
2025-12-23 16:27:54 -0800

    profile page and saved page created

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from 'react-native'
+import { icons } from '@/constants/icons'
+import { Image, Text, View } from 'react-native'
 
 const saved = () => {
   return (
-    <View>
-      <Text>saved</Text>
-    </View>
+    <View className='bg-primary flex-1 px-10'>
+          <View className='flex items-center justify-center flex-1 flex-col gap-5'>
+            <Image source={icons.save} className='size-10' tintColor="#fff"></Image>
+            <Text className='text-gray-500 text-base'>Save</Text>
+            </View>
+          </View>
   )
 }
 
